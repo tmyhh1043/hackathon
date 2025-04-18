53b8fb7c4a2a791a1fcbe4bd4ef127431e732c4d
tmyhh1043 <tmyhh1106@outlook.jp>
2025-04-18 16:48:19 +0900

    転機を追加

--- attendance/views.py
+++ attendance/views.py
@@ -3,16 +3,25 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import AttendanceLog
 from django.contrib.auth import logout
+from .weather import get_osaka_weather
 
 
 @login_required
 def top_view(request):
-    return render(request, 'attendance/top.html')
+    # 大阪の天気情報を取得
+    weather_info = get_osaka_weather()
+    
+    # 既存の処理と統合
+    return render(request, 'attendance/top.html', {
+        'weather_info': weather_info
+    })
+
 
 @login_required
 def history_view(request):
     logs = AttendanceLog.objects.filter(user=request.user).order_by('-timestamp')
     return render(request, 'attendance/history.html', {'logs': logs})
+
 
 @login_required
 def record_attendance(request, type):
@@ -20,6 +29,7 @@ def record_attendance(request, type):
         AttendanceLog.objects.create(user=request.user, type=type)
     return redirect('history')  # 登録後は履歴ページへリダイレクト
 
+
 @login_required
-def dashboard_view(request, type):
-    return render(request, 'attendance/dashboard.html')  
+def dashboard_view(request):
+    return render(request, 'attendance/dashboard.html')
